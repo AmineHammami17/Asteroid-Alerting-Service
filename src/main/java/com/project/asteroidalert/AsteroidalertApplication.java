fe6a2c8aa5a510899da40893a023351a8c154f92
AmineHammami17 <lorenzoamine17@gmail.com>
2025-05-26 16:07:58 +0100

    Initial Commit

---
+++ src/main/java/com/project/asteroidalert/AsteroidalertApplication.java
@@ -0,0 +1,13 @@
+package com.project.asteroidalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteroidalertApplication.class, args);
+	}
+
+}
